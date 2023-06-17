9926698277cfa933db2ee7e24a94a53124af5810
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-06-17 10:30:10 +0500

    Action: Include all the actions in redux

---
+++ src/redux/actions/ActionType.js
@@ -0,0 +1,8 @@
+//Declare all the Todo
+
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const EDIT_TODO = "EDIT_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const COMPLETED_TODO = "COMPLETED_TODO";
+export const CLEAR_ALL_TODO = "CLEAR_ALL_TODO";
---
+++ src/redux/actions/Index.js
@@ -0,0 +1,69 @@
+//Logic of the Todos
+import {
+  ADD_TODO,
+  EDIT_TODO,
+  DELETE_TODO,
+  UPDATE_TODO,
+  COMPLETED_TODO,
+  CLEAR_ALL_TODO,
+} from "./ActionType";
+
+//Action of Add Todo
+export const addTodo = (todo) => {
+  return {
+    type: ADD_TODO,
+    payload: {
+      id: Date.now(),
+      title: todo?.title,
+      description: todo?.description,
+    },
+  };
+};
+
+//Action of Delete Todo
+export const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+//Action of Clear Todo
+export const clearTodo = () => {
+  return {
+    type: CLEAR_ALL_TODO,
+  };
+};
+
+//Action of Edit Todo
+export const editTodo = (id) => {
+  return {
+    type: EDIT_TODO,
+    payload: {
+      id: id,
+    },
+    isEdit: true,
+  };
+};
+
+//Action of Update Todo
+export const updateTodo = (id, todo) => {
+  return {
+    type: UPDATE_TODO,
+    payload: {
+      todoID: id,
+      todoTitle: todo?.title,
+      todoDescription: todo?.description,
+    },
+  };
+};
+
+//Aciton of Completed Todo
+export const completedTodo = (id) => {
+  return {
+    type: CLEAR_ALL_TODO,
+    payload: {
+      selectedTodoID: id,
+    },
+  };
+};
